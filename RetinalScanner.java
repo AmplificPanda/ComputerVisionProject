31db11af01d9a5fd5e3e901630e43a70336e46ff
RTTGOD <avianrtt71@gmail.com>
2022-06-14 19:01:10 +1200

    Basics

---
+++ RetinalScanner.java
@@ -0,0 +1,16 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
+
+public class RetinalScanner {
+    //Compulsory for OpenCV
+    static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+
+    public static void main(String[] args) {
+    //main method
+
+
+    }
+}
+

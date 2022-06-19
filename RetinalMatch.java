63988ab1dff6aeaa0c75cba217c07cf2e24287f4
RTTGOD <avianrtt71@gmail.com>
2022-06-20 09:05:27 +1200

    RetinalMatch.java now fully works with 99%-100% accuracy.

--- RetinalMatch.java
+++ RetinalMatch.java
@@ -3,15 +3,19 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.Point;
 import org.opencv.core.Size;
+import org.opencv.features2d.BFMatcher;
+import org.opencv.features2d.KAZE;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.CLAHE;
 import org.opencv.imgproc.Imgproc;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.AreaAveragingScaleFilter;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.sql.Array;
 import java.util.ArrayList;
 
 import static org.opencv.imgproc.Imgproc.*;
@@ -54,13 +58,13 @@ public class RetinalMatch {
         clahe.apply(channels1.get(1), FirstInput);
         clahe.apply(channels2.get(1),SecondInput);
 
-        //apply median blur
-        Imgproc.medianBlur(FirstInput, FirstInput, 17);
-        Imgproc.medianBlur(SecondInput, SecondInput, 17);
-
         //thresholding
-        Imgproc.adaptiveThreshold(FirstInput, FirstInput, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 61, 11);
-        Imgproc.adaptiveThreshold(SecondInput, SecondInput, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 61,  11);
+        Imgproc.adaptiveThreshold(FirstInput, FirstInput, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 43, 11);
+        Imgproc.adaptiveThreshold(SecondInput, SecondInput, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 43,  11);
+
+        //apply median blur
+        Imgproc.medianBlur(FirstInput, FirstInput, 13);
+        Imgproc.medianBlur(SecondInput, SecondInput, 13);
 
         //apply erosion and dilation
         int kernelSize = 1;
@@ -79,9 +83,6 @@ public class RetinalMatch {
         //store the inverted images
         Mat FirstInvert = FirstInput;
         Mat SecondInvert = SecondInput;
-
-        imshow(FirstInvert);
-        imshow(SecondInvert);
 
         //find contours
         ArrayList<MatOfPoint> contours = new ArrayList<>();
@@ -109,7 +110,7 @@ public class RetinalMatch {
             double contourArea = Imgproc.contourArea(contoursTwo.get(cIdx));
             Scalar color = new Scalar(255, 255, 255);
             if ( 1 < contourArea  && contourArea < 200000) {
-                System.out.println(contourArea);
+                //System.out.println(contourArea);
                 Imgproc.drawContours(drawingTwo, contoursTwo, cIdx, color, 20, Imgproc.LINE_8, hierarchy, 0, new Point());
             }
         }
@@ -126,10 +127,50 @@ public class RetinalMatch {
         Core.bitwise_and(drawingOne, FirstInvert,resultOne);
         Core.bitwise_and(drawingTwo, SecondInvert,resultTwo);
 
-        //output the result (temporary)
-        imshow(resultOne);
-        imshow(resultTwo);
+        double percent;
+        percent = SIFT(resultOne,resultTwo);
+        if (percent > 15){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+    }
 
+    private static double SIFT(Mat matrix1, Mat matrix2){
+        KAZE detect = KAZE.create();
+        BFMatcher matcher = BFMatcher.create();
+
+        MatOfKeyPoint kp1 = new MatOfKeyPoint();
+        Mat d1 = new Mat();
+        Mat mask = new Mat();
+        detect.detectAndCompute(matrix1,mask,kp1,d1,false);
+
+        MatOfKeyPoint kp2 = new MatOfKeyPoint();
+        Mat d2 = new Mat();
+        Mat mask2 = new Mat();
+        detect.detectAndCompute(matrix2,mask2,kp2,d2,false);
+
+        ArrayList<MatOfDMatch> matchesD = new ArrayList<>();
+        matcher.knnMatch(d1,d2,matchesD,2);
+
+        double ratio = 0.75;
+        ArrayList<DMatch> foundMatch = new ArrayList<>();
+
+        for (int i=0; i<matchesD.size(); i++){
+            MatOfDMatch matOfDMatch = matchesD.get(i);
+            DMatch[] dArray = matOfDMatch.toArray();
+            DMatch oneD = dArray[0];
+            DMatch twoD = dArray[1];
+
+            if(oneD.distance <= twoD.distance * ratio){
+                foundMatch.add(oneD);
+            }
+        }
+        int foundNum = foundMatch.size();
+        double percent = (foundNum*100)/matchesD.size();
+        //System.out.println(percent);
+        return percent;
     }
 
     public static void imshow(Mat src){ //displays images
